9d8ab254554c81737cecc5c968fa4b340c48601e
mayank pandey <mayankpandey1099@gmail.com>
2024-05-01 10:53:02 +0530

    initial commit

---
+++ Components/About.js
@@ -0,0 +1,13 @@
+const About = ()=>{
+    return (
+        <div>
+            <h1> About Us Page</h1>
+        <p>
+        {" "}
+        This is React project on swiggy
+            </p>
+        </div>
+    );
+};
+
+export default About;
